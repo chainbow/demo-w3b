8c4f620af4746e3c3969cc98ec94ab00db98fdd0
Amence <paopaoxiaoyanjing@gmail.com>
2023-02-04 01:44:54 +0900

    fix: 启动完成之前不清除

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ docker rmi auth_client:latest
 docker build -f ./Dockerfile -t auth_client:latest .
 docker compose down
 docker compose up -d
-docker system prune -f --all
 docker logs -f auth_client
