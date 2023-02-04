2fcf2a66b4ff0cd2f2f822e22aeabfe62a779990
Amence <paopaoxiaoyanjing@gmail.com>
2023-02-04 14:56:43 +0900

    fix: 注释签名

--- src/pages/api/checkSig.ts
+++ src/pages/api/checkSig.ts
@@ -2,18 +2,19 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import ethSigUtil from "eth-sig-util";
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const {address, sig, provider} = req.query;
   const msgParams = {data: "message", sig: sig} as any;
-  const checkSigResult = ethSigUtil.recoverPersonalSignature(msgParams);
-
-  if (String(address).toLowerCase() !== checkSigResult) {
-    res.status(500).send({success: false});
-    return;
-  }
+  // const checkSigResult = ethSigUtil.recoverPersonalSignature(msgParams);
+  //
+  // if (String(address).toLowerCase() !== checkSigResult) {
+  //   res.status(500).send({success: false});
+  //   return;
+  // }
   const response = await axios.get(`http://wallet3.net/api/address?accountId=${ address }&provider=${ provider }`,
     {headers: {"apikey": process.env.API_KEY}});
   const result = response.data;
