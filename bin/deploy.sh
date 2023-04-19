c9acadff2c09a8a09dfee228f3df59748e0962f9
suhao <suhao@chainbow.io>
2023-04-19 15:59:29 +0800

    fix: deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 docker system prune -f --all
-docker compose down
-docker build -f ./Dockerfile -t demo-w3b:latest .
-docker network create shared
-docker compose down
-docker compose up -d
+docker-compose down
+docker-compose up -d
 docker logs -f demo-w3b
